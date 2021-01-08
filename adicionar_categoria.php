4e1070ab698ae7abc8b71763dc272c9a54f7174d
Alessandro <30908676+alessandroce@users.noreply.github.com>
2021-01-08 17:39:47 -0300

    alteração

---
+++ adicionar_categoria.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	 <meta charset="utf-8">
+	<title>Formulário de Cadastro</title>
+	
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+   
+  <style type="text/css">
+
+      #tamanhoContainer{
+        width: 500px; 
+
+      }
+
+      #botao {
+        background-color: #FF1168; /*cor de fundo*/
+        color: #FFFFFF; /*cor da letra*/
+      }
+    </style>
+      
+</head>
+<body>
+
+<div class="container" style="margin-top: 40px; width: 500px">
+
+  <div style="text-align: right">
+    <a href="index.php" role="button" class="btn btn-sm btn-primary">Voltar</a>
+  </div>
+  
+	<h4>Cadastro de Categoria</h4>
+
+<form  action="_inserir_categoria.php" method="post" style="margin-top: 20px">
+
+  <div class="form-group">
+    <label>Nome da Categoria</label>
+    <input type="text" class="form-control" name="categoria" placeholder="Insira o nome da categoria" required autocomplete="off">
+  </div>
+
+  <div style="text-align: right;">
+  <button id="botao" type="submit" class="btn btn-sm" >Cadastrar</button>
+  </div>	
+</form>
+</div>
+
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -7,10 +7,16 @@
 
 </head>
 <body>
+
+
+
+
   <div class="container" style="margin-top: 100px">
   	
 
-<div class="row">
+  <div class="row">
+
+
   <div class="col-sm-6">
     <div class="card">
       <div class="card-body">
@@ -29,7 +35,35 @@
       </div>
     </div>
   </div>
-</div>
+
+
+  <div class="col-sm-6" style="margin-top: 20px">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Adicionar Categoria</h5>
+        <p class="card-text">Opção para adicionar categoria.</p>
+        <a href="adicionar_categoria.php" class="btn btn-primary">Cadastrar Produto</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6" style="margin-top: 20px">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Adicionar Fornecedor</h5>
+        <p class="card-text">Opção para adicionar fornecedores.</p>
+        <a href="#" class="btn btn-primary">Cadastrar</a>
+      </div>
+    </div>
+  </div>
+
+
+  </div>
+
+
+
+  </div>
+
+
 
   </div>
 
